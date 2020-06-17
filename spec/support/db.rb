25af7c9e967188b85f3ef0ad00b3feaa6ab45008
Nitish Parkar <nitish@getsimpl.com>
2020-06-17 16:18:23 +0530

    Log before and after db tests

--- spec/support/db.rb
+++ spec/support/db.rb
@@ -1,11 +1,23 @@
+FileUtils.mkdir_p('log')
+require 'logger'
+DB.loggers << Logger.new('log/sequel.log')
+
 RSpec.configure do |c| 
   c.before(:suite) do
     Sequel.extension :migration 
     Sequel::Migrator.run(DB, 'db/migrations') 
     DB[:expenses].truncate
-  end 
+  end
+
+  c.before(:example, :db) do |example|
+    DB.log_info("Starting example: #{example.full_description}")
+  end
+
+  c.after(:example, :db) do |example|
+    DB.log_info("Ending example: #{example.full_description}")
+  end
 
   c.around(:example, :db) do |example| 
-    # DB.transaction(rollback: :always) { example.run }
+    DB.transaction(rollback: :always) { example.run }
   end  
 end
